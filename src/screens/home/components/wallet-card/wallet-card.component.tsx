3ab1577b5f3858ddb2e2fc664bc72a1df0799d86
koushith <koushith97@gmail.com>
2022-03-04 01:28:19 +0530

    adds shimmer effect for cards

--- src/screens/home/components/wallet-card/wallet-card.component.tsx
+++ src/screens/home/components/wallet-card/wallet-card.component.tsx
@@ -4,26 +4,40 @@ import { WalletCardProps } from './wallet-card.component.props';
 import { Link } from 'react-router-dom';
 import { RoutePath } from 'navigation/route-path';
 import { getStatusInfo, getUserInfo } from './wallet-card.component.utils';
+import { WalletCardShimmer } from './wallet-card-shimmer.component';
 
 export const WalletCard: React.FC<WalletCardProps> = (props) => {
-  const { walletName, roleName, status  } = props;
+  const { walletName, roleName, status, shimmer } = props;
 
   return (
-    <StyledWalletCard hCenter vCenter>
+    <>
+      {shimmer ? (
+        <WalletCardShimmer />
+      ) : (
+        <>
+          <StyledWalletCard hCenter vCenter>
             <Link to={RoutePath.createWallet}>
               <IconSvg name='safientWallet' size='large' />
             </Link>
             <WalletText variant='content' text={walletName} />
             <MetaInfo row marginTop={2} color='bottomAccent' padding={2.2} align={'center'}>
-        <Box align={'center'} padding={1} row >
-        <IconSvg name={getUserInfo(roleName).iconName} size='small' />
-          <Text paddingLeft={1} variant='small' text={getUserInfo(roleName).text} />
-        </Box>
-        <Box align={'center'} paddingLeft={6} row>
-        <IconSvg name={getStatusInfo(status).iconName} size='medium' />
-          <Text paddingLeft={1} variant='small' text={getStatusInfo(status).text} color={getStatusInfo(status).color} />
-        </Box>
-      </MetaInfo>
-    </StyledWalletCard>
+              <Box align={'center'} padding={1} row>
+                <IconSvg name={getUserInfo(roleName).iconName} size='small' />
+                <Text paddingLeft={1} variant='small' text={getUserInfo(roleName).text} />
+              </Box>
+              <Box align={'center'} paddingLeft={6} row>
+                <IconSvg name={getStatusInfo(status).iconName} size='medium' />
+                <Text
+                  paddingLeft={1}
+                  variant='small'
+                  text={getStatusInfo(status).text}
+                  color={getStatusInfo(status).color}
+                />
+              </Box>
+            </MetaInfo>
+          </StyledWalletCard>
+        </>
+      )}
+    </>
   );
 };
