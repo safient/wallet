2f410feb02e5f135b8be67ae47d2bb4efcfe80d6
Koushith <koushith97@gmail.com>
2022-03-11 16:04:31 +0530

    adds settings page (#31)

--- src/screens/create-wallet/create-wallet.screen.tsx
+++ src/screens/create-wallet/create-wallet.screen.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { NoticeLoader } from "components/primitive";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { NoticeLoader } from 'components/primitive';
 import {
   FormContainer,
   HomeScreenContainer,
@@ -10,41 +10,40 @@ import {
   WalletCreateFormContainer,
   WalletCreateFormBox,
   WalletCreateText,
-} from "./create-wallet.screen.styles";
+} from './create-wallet.screen.styles';
 
-import { RoutePath } from "navigation/route-path";
-import { useServices } from "services";
-import { useStores } from "store";
-import { observer } from "mobx-react-lite";
+import { RoutePath } from 'navigation/route-path';
+import { useServices } from 'services';
+import { useStores } from 'store';
+import { observer } from 'mobx-react-lite';
 
 export const CreateWalletScreen = observer(() => {
   const { safeService, walletService } = useServices();
   const { safeStore } = useStores();
   let history = useHistory();
 
-  const [walletName, setWalletName] = useState("");
-  const [walletDescription, setWalletDescription] = useState("");
+  const [walletName, setWalletName] = useState('');
+  const [walletDescription, setWalletDescription] = useState('');
   const [walletBeneficiary, setWalletBeneficiary] = useState(
-    "did:key:z6MkvbAdUeiQoeLbcvV3tmnAjTeV3WAtXrxqu2Tbx1PFTpH9"
+    'did:key:z6MkvbAdUeiQoeLbcvV3tmnAjTeV3WAtXrxqu2Tbx1PFTpH9'
   );
 
   const createSafe = async () => {
     try {
       safeStore.setFetching(true);
 
-      const wallet = await walletService.create()
-      await walletService.info()
+      const wallet = await walletService.create();
+      await walletService.info();
 
-      if(wallet.hasData()) {
+      if (wallet.hasData()) {
+        const safe = await safeService.create(walletBeneficiary, wallet.data!.mnemonic, false);
+        if (safe.hasData()) {
+          history.push(RoutePath.walletOverview);
+        } else {
+          history.push(RoutePath.createWallet);
+        }
+      }
 
-      const safe = await safeService.create(walletBeneficiary, wallet.data!.mnemonic, false);
-      if (safe.hasData()) {
-        history.push(RoutePath.walletOverview);
-      } else {
-        history.push(RoutePath.createWallet);
-      }
-      }
-     
       safeStore.setFetching(false);
     } catch (e) {
       console.log(e);
@@ -55,51 +54,50 @@ export const CreateWalletScreen = observer(() => {
     <HomeScreenContainer>
       {safeStore.fetching && (
         <NoticeLoader
+
           label={{ tx: "wallet.creatingLabel" }}
+
           helperText={{
-            text:
-              "Please sign the signature on MetaMask. This may take a couple of seconds ...",
+            text: 'Please sign the signature on MetaMask. This may take a couple of seconds ...',
           }}
         />
       )}
-      <Title variant="contentHeader" tx="common.createWallet" />
+      <Title variant='contentHeader' tx='common.createWallet' />
 
       <WalletCreateFormContainer>
         <FormContainer>
-          <WalletCreateText
-            variant="contentHeader"
-            center
-            tx="wallet.enterDetails"
-          />
+          <WalletCreateText variant='contentHeader' center tx='wallet.enterDetails' />
 
           <WalletCreateFormBox>
             <StyledInput
-              type="text"
-              label="Wallet Name"
-              placeholder="John Doe Wallet"
+              type='text'
+              label='Wallet Name'
+              placeholder='John Doe Wallet'
               onChange={(e: any) => {
                 setWalletName(e.target.value);
               }}
             />
             <StyledInput
-              type="text"
-              label="Wallet Description"
-              placeholder="johndoe@safeint.com"
+              type='text'
+              label='Wallet Description'
+              placeholder='johndoe@safeint.com'
               onChange={(e: any) => setWalletDescription(e.target.value)}
             />
             <StyledInput
-              type="text"
-              label="Beneficiary"
-              placeholder="johndoe@safient.com"
+              type='text'
+              label='Beneficiary'
+              placeholder='johndoe@safient.com'
               onChange={(e: any) => setWalletBeneficiary(e.target.value)}
             />
           </WalletCreateFormBox>
 
           <StyledButton
+
             variant="primary"
             label={{ text: safeStore.fetching ? "Creating.." : "Create 🙌" }}
+
             onClick={createSafe}
-            color="primaryGradient"
+            color='primaryGradient'
           />
         </FormContainer>
       </WalletCreateFormContainer>
--- src/screens/wallet-settings/wallet-settings.screen.tsx
+++ src/screens/wallet-settings/wallet-settings.screen.tsx
@@ -1,3 +1,49 @@
+import { Box, IconSvg } from 'components/primitive';
+import {
+  BackButtonContainer,
+  BeneficiaryContainer,
+  FormContainer,
+  Label,
+  StyledButton,
+  StyledInput,
+  WalletSettingsFormBox,
+  WalletSettingsFormContainer,
+  WalletSettingsText,
+  SignnalingInput,
+  AddIconContainer,
+} from './wallet-settings.screen.styles';
+
 export const WalletSettingsScreen = () => {
-  return <>Wallet settings screen</>;
+  return (
+    <WalletSettingsFormContainer>
+      <FormContainer>
+        <BackButtonContainer>
+          <IconSvg name='arrowLeft' />
+        </BackButtonContainer>
+        <WalletSettingsText variant='contentHeader' center text='Settings' />
+
+        <WalletSettingsFormBox>
+          <StyledInput type='text' label='Wallet Name' placeholder='A test Wallet' />
+
+          <BeneficiaryContainer>
+            <Label>Add a beneficiary</Label>
+            <AddIconContainer align={'end'}>
+              <IconSvg name='add' size='large' />
+            </AddIconContainer>
+          </BeneficiaryContainer>
+          <Box marginTop={-2} marginBottom={1.2}>
+            <StyledInput type='text' placeholder='johndoe@safeint.com' />
+          </Box>
+
+          <Label>Advanced options</Label>
+          <Box row hCenter marginTop={-2} justify={'between'}>
+            <Label>Signaling Period</Label>
+            <SignnalingInput type='text' placeholder='10 days' />
+          </Box>
+        </WalletSettingsFormBox>
+
+        <StyledButton variant='primary' label={{ text: 'Save' }} onClick={() => ''} color='primaryGradient' />
+      </FormContainer>
+    </WalletSettingsFormContainer>
+  );
 };
