433893284b1267ae80a4880add841b0f6afbf011
koushith <koushith97@gmail.com>
2022-03-15 23:28:03 +0530

    adds localization

--- src/screens/notifications/notifications.screen.tsx
+++ src/screens/notifications/notifications.screen.tsx
@@ -16,45 +16,19 @@ export const NotificationsScreen = () => {
         <BackButtonContainer>
           <IconSvg name='arrowLeft' />
         </BackButtonContainer>
-        <Text variant='contentHeader' center text='Notifications' />
+        <Text variant='contentHeader' center tx='notifications.notification' />
         <Box marginTop={3.2}>
           <Activities>
             <IconSvg name='document' size='xLarge' />
 
             <Box>
-              <Text variant='small' text='Someone is claiming your wallet' color='textLight' />
+              <Text variant='small' tx='notifications.claimingText' color='textLight' />
               <Text variant='small' text='2 days left to respond' color='textLighter' />
             </Box>
 
             <StyledButton row hCenter vCenter gap={0.4} color='applicationBackground'>
               <IconSvg name='delete' />
-              Cancel Claim
-            </StyledButton>
-          </Activities>{' '}
-          <Activities>
-            <IconSvg name='document' size='xLarge' />
-
-            <Box>
-              <Text variant='small' text='Someone is claiming your wallet' color='textLight' />
-              <Text variant='small' text='2 days left to respond' color='textLighter' />
-            </Box>
-
-            <StyledButton row hCenter vCenter gap={0.4} color='applicationBackground'>
-              <IconSvg name='delete' />
-              Cancel Claim
-            </StyledButton>
-          </Activities>{' '}
-          <Activities>
-            <IconSvg name='document' size='xLarge' />
-
-            <Box>
-              <Text variant='small' text='Someone is claiming your wallet' color='textLight' />
-              <Text variant='small' text='2 days left to respond' color='textLighter' />
-            </Box>
-
-            <StyledButton row hCenter vCenter gap={0.4} color='applicationBackground'>
-              <IconSvg name='delete' />
-              Cancel Claim
+              {t('notifications.cancelClaim')}
             </StyledButton>
           </Activities>{' '}
           <Activities>
