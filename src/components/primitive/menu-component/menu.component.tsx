e848c53047fdb3abe4953328542d90ffe64a0772
Koushith <koushith97@gmail.com>
2022-03-18 10:41:16 +0530

    #32 - Create a Menu Component (#44)
    * adds menu dropdown
    
    * removes inline styling
    
    * code cleanup and adds localization
    
    * removed darkmode option and minor code refactors
    
    * fixes menu modal
    
    Co-authored-by: Koshik Raj <koshik.raj@gmail.com>

---
+++ src/components/primitive/menu-component/menu.component.tsx
@@ -0,0 +1,68 @@
+// @ts-nocheck
+import { useState } from 'react';
+import { RoutePath } from 'navigation/route-path';
+import { ModalProvider } from 'styled-react-modal';
+import { Text, Image, Box } from 'components/primitive';
+import { FadingBackground } from '../generic-modal/generic-modal.component.style';
+import { NameContainer, StyledModal, NavLabelContainer, MenuContainer } from './menu.component.styles';
+import { IconSvg } from '../icon-svg/icon-svg.component';
+import { MenuComponentProps } from './menu.component.props';
+import { useHistory } from 'react-router-dom';
+import { useStores } from 'store';
+
+export const Menu = (props: any) => {
+
+  const history = useHistory()
+  const { accountStore } = useStores();
+  const { show, onClose, noClose } = props;
+  const [isOpen, setIsOpen] = useState(show);
+  const [opacity, setOpacity] = useState(0);
+
+  const toggleModal = (e: any) => {
+    setOpacity(0);
+    setIsOpen(!isOpen);
+    if (onClose) onClose();
+  };
+
+  const afterOpen = () => {
+    setOpacity(1);
+  };
+
+  const beforeClose = () => {
+    setOpacity(0);
+  };
+
+  return (
+    <MenuContainer>
+      <ModalProvider backgroundComponent={FadingBackground}>
+        <StyledModal
+          isOpen={isOpen}
+          afterOpen={afterOpen}
+          beforeClose={beforeClose}
+          onBackgroundClick={noClose ? () => {} : toggleModal}
+          onEscapeKeydown={noClose ? () => {} : toggleModal}
+          backgroundProps={{ opacity }}
+        >
+          <NameContainer>
+            <Image name='user' rounded width={4.4} />
+            <Box>
+              <Text variant='small' text={accountStore.safientUser.name} color='textLight' />
+              <Text variant='small' text={accountStore.safientUser.email} color='textLighter' />
+            </Box>
+          </NameContainer>
+          <NavLabelContainer>
+            <Box row gap={1} hCenter>
+              <IconSvg name='user' />
+              <Text variant='small' tx='menuComponent.profile' />
+            </Box>
+
+            <Box row gap={1} hCenter onClick={()=> history.push(RoutePath.login)}>
+              <IconSvg name='logout' />
+              <Text variant='small' tx='menuComponent.logout' />
+            </Box>
+          </NavLabelContainer>
+        </StyledModal>
+      </ModalProvider>
+    </MenuContainer>
+  );
+};
--- src/components/primitive/top-bar/top-bar.component.tsx
+++ src/components/primitive/top-bar/top-bar.component.tsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { RoutePath } from 'navigation/route-path';
 import { Image, Avatar } from 'components/primitive';
 import { TopBarContainer, LogoWrapper, BoxGroup, NotificationIcon } from './top-bar.component.styles';
-import { RoutePath } from 'navigation/route-path';
-import { Link } from 'react-router-dom';
+import { Menu } from '../menu-component/menu.component';
 
 export const TopBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <TopBarContainer align={'center'} justify={'center'} hCenter vCenter row>
-      <LogoWrapper hCenter vCenter>
-      <Link to={RoutePath.home} >
-        <Image name='safientWalletLogo' width={18} />
-      </Link>
-      </LogoWrapper>
-      <BoxGroup row>
-        <NotificationIcon name='notification' size='small' />
-        <Avatar name='user' size='small' />
-      </BoxGroup>
-    </TopBarContainer>
+    <>
+      <TopBarContainer align={'center'} justify={'center'} hCenter vCenter row>
+        <LogoWrapper hCenter vCenter>
+          <Link to={RoutePath.home}>
+            <Image name='safientWalletLogo' width={18} />
+          </Link>
+        </LogoWrapper>
+        <BoxGroup row>
+          <NotificationIcon name='notification' size='small' />
+          <Avatar name='user' size='small' onClick={() => setShowMenu(true)} style={{ cursor: 'pointer' }} />
+        </BoxGroup>
+      </TopBarContainer>
+      {showMenu && <Menu show onClose={setShowMenu}/>}
+    </>
   );
 };
