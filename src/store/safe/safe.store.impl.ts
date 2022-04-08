80d6e2bd88140d40991a7e6e808f483a7d352515
Koushith <koushith97@gmail.com>
2022-04-08 18:47:12 +0530

    #62 - Disable button issue in registration screen #62 (#68)
    * adds disable support to box component
    
    * adds disable support to box component

--- src/store/safe/safe.store.impl.ts
+++ src/store/safe/safe.store.impl.ts
@@ -1,18 +1,14 @@
 import { action, makeObservable, observable } from 'mobx';
 import { SafeStore } from './safe.store';
 import { StoreImpl } from '../store/store.impl';
-import { Types, Enums} from '@safient/core';
-import { WalletSecret, WalletInfo } from '../../utils/Wallet'
-
+import { Types, Enums } from '@safient/core';
+import { WalletSecret, WalletInfo } from '../../utils/Wallet';
 
 export class SafeStoreImpl extends StoreImpl implements SafeStore {
-
-
   _safe?: Types.Safe;
   _walletSecret?: WalletSecret;
   _walletInfo?: WalletInfo;
   _safeRole?: string;
-
 
   constructor() {
     super();
@@ -25,38 +21,34 @@ export class SafeStoreImpl extends StoreImpl implements SafeStore {
     });
   }
 
-  get safe(): Types.Safe|undefined {
+  get safe(): Types.Safe | undefined {
     return this._safe;
   }
 
-  get walletInfo(): WalletInfo|undefined {
+  get walletInfo(): WalletInfo | undefined {
     return this._walletInfo;
   }
 
-  get walletSecret(): WalletSecret|undefined {
+  get walletSecret(): WalletSecret | undefined {
     return this._walletSecret;
   }
 
   setSafe(safe: Types.Safe) {
     this._safe = safe;
- }
-
- setRole(role: string) {
-   this._safeRole = role;
- }
-
- get role(): string|undefined {
-   return this._safeRole;
- }
-
-  setWallet(info: WalletInfo, secret?: WalletSecret) {
-
-    this._walletInfo = info;
-    this._walletSecret = secret;
-    
-   }
-
-   async resetStore() {
   }
 
+  setRole(role: string) {
+    this._safeRole = role;
+  }
+
+  get role(): string | undefined {
+    return this._safeRole;
+  }
+
+  setWallet(info: WalletInfo, secret?: WalletSecret) {
+    this._walletInfo = info;
+    this._walletSecret = secret;
+  }
+
+  async resetStore() {}
 }
