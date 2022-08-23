26de5360400c396470d3fbce81de68ec21298dc5
koushith <koushith97@gmail.com>
2022-08-24 04:06:08 +0530

    adds progress status

--- src/store/store/store.impl.ts
+++ src/store/store/store.impl.ts
@@ -1,15 +1,18 @@
-import {action, computed, makeObservable, observable} from 'mobx'
-import {Store} from './store'
+import { action, computed, makeObservable, observable } from "mobx";
+import { Store } from "./store";
 
 export class StoreImpl implements Store {
-  protected _fetching: boolean
-  protected _error: string
-  protected _code: number
+  protected _fetching: boolean;
+  protected _error: string;
+  protected _code: number;
+  protected _statusMessage: string;
 
   constructor() {
-    this._fetching = false
-    this._error = ''
-    this._code = 0
+    this._fetching = false;
+    this._error = "";
+    this._code = 0;
+    // todo- exp-remove if it didn't work
+    this._statusMessage = "";
 
     makeObservable<StoreImpl, any>(this, {
       _fetching: observable,
@@ -20,33 +23,42 @@ export class StoreImpl implements Store {
       errorCode: computed,
       setFetching: action,
       setError: action,
-    })
+      _statusMessage: observable,
+      setStatusMessage: action,
+    });
   }
 
   get fetching() {
-    return this._fetching
+    return this._fetching;
   }
 
   get error() {
-    return this._error
+    return this._error;
   }
 
   get errorCode() {
-    return this._code
+    return this._code;
+  }
+
+  getStatusMessage() {
+    return this._statusMessage;
   }
 
   public setFetching(flag: boolean) {
-    this._fetching = flag
+    this._fetching = flag;
 
     if (flag) {
-      this._error = ''
+      this._error = "";
     }
   }
 
   public setError(message?: string, code?: number) {
-    this._error = message || ''
-    this._code = code || 0
-    this.setFetching(false)
+    this._error = message || "";
+    this._code = code || 0;
+    this.setFetching(false);
   }
 
+  public setStatusMessage(status?: string) {
+    this._statusMessage = status || "";
+  }
 }
